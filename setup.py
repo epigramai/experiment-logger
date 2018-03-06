7039e7a5d81d3e87867b0af616f92d31f24374f4
Stian Lind Petlund <stianlp@gmail.com>
2018-03-06 13:52:23 +0100

    Added setup.py file for packaing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='experiment-logger',
+    version='0.0',
+    description='A package for logging ML experiments',
+    url='https://github.com/epigramai/experiment-logger',
+    author='Oliver Urs Lenz',
+    author_email='oliver@epigram.ai',
+    packages=find_packages('.')
+)
